8a714668b844cfaa7ae11758519ae5ee8d955ea6
mlyk1234 <32647481+morgan-redbite@users.noreply.github.com>
2024-11-18 16:21:51 +0800

    Update mongoose

--- src/database/index.ts
+++ src/database/index.ts
@@ -2,7 +2,8 @@ import mongoose from "mongoose";
 
 const mongooseConnect = async () => {
   try {
-    await mongoose.connect('mongodb://localhost:27017/webmaster_sniper');
+    // await mongoose.connect('mongodb://localhost:27017/webmaster_sniper');
+    await mongoose.connect('mongodb+srv://root:rootpass@cluster0.vlwon.mongodb.net/admin?authSource=admin&replicaSet=atlas-6i8mul-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true')
     console.log('✅ MongoDB connected');
   } catch (error) {
     console.log('❌ MongoDB connection failed');
